a90ebb067725b6daea92e68de023f54be66458b1
Sulthonhanan <hananakhnaf31@gmail.com>
2024-05-21 14:45:25 +0700

    Add project files.

---
+++ Member Inisialization list/Member Inisialization list.cpp
@@ -0,0 +1,25 @@
+// Member Inisialization list.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+    const int id;
+    string nama;
+    float nilai;
+
+public:
+    mahasiswa (int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) { //Member
+    //Definisi
+    }
+};
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
